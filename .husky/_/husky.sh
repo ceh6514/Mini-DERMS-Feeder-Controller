d221bdc8dc9213f0cebdb8748057d24870b436b8
ceh6514 <ceh6514@mavs.uta.edu>
2026-01-06 14:39:21 -0600

    chore: sync lockfile and stop tracking node_modules

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,15 +1,9 @@
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
 #!/usr/bin/env sh
-# shellcheck shell=sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-if [ "$HUSKY" = "0" ]; then
-  exit 0
-fi
-
-command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
-if ! command_exists sh; then
-  echo "husky - shell not found"
-  exit 1
-fi
+They WILL FAIL in v10.0.0
+"
